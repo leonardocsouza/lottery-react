2398801a0835e1ac6aaae83bcc440444a36756bb
Leonardo Souza <leonardocsouza@gmail.com>
2018-03-20 09:48:22 -0400

    refactor constructor with ES2016 syntax

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      manager: '',
-    };
-  }
+  state = {
+    manager: ''
+  };
 
   async componentDidMount() {
     // We don't need to pass from: account below because
